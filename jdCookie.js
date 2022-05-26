627676f4673df05c7b054eef775e804ff5471f6e
beichen100 <42291236+beichen100@users.noreply.github.com>
2022-05-26 11:24:04 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJijNtaADAkMn8FB-DethW6p0Hql16UZc4JswHy7OIvM0cruuxc5tosCkC8TL8uvb4s2P2YPqw; pt_pin=jd_kRZdueUOymYr;',
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
